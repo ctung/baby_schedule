f5af7ae3968d948eb1f00005abc7bbe430446f00
Chuck Tung <chuck.tung@gmail.com>
2016-07-10 00:31:20 +0000

    remove uneeded js libs

--- scripts.php
+++ scripts.php
@@ -2,8 +2,6 @@
 <script   src="https://code.jquery.com/jquery-2.2.4.min.js"   integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="   crossorigin="anonymous"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="js/bootstrap.min.js"></script>
-<script src="js/bootstrap-datepicker.min.js"></script>
-<script src="js/bootstrap-clockpicker.min.js"></script>
 <script src="js/nouislider.min.js"></script>
 
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
